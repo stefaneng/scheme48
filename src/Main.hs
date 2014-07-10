fc3aee92101582137bc6d9aa454e0ab3c87be13f
Stefan Eng <stefaneng13@gmail.com>
2014-07-09 21:01:21 -0700

    First Steps: Improved hello world
    Reads the first argument from the command line, printing it out along
    with a message when run.

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,11 @@
-main :: IO()
-main = putStrLn "Hello world!"
+module Main where
+
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStr "Hello"
+  if length args > 0
+  then putStrLn (", " ++ args !! 0)
+  else putStrLn ""
