b41276467ceb086a10bceeeedbd719e38123080c
Stefan Eng <stefaneng13@gmail.com>
2014-07-09 21:09:37 -0700

    First Steps: Exercise 1
    Reads in two arguments from the command line and prints out a message
    using both.

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,17 @@ module Main where
 
 import System.Environment (getArgs)
 
+argumentMessage :: [String] -> String
+argumentMessage args = ", " ++ case length args of
+                                 0 -> ""
+                                 1 -> args !! 0
+                                 2 -> args !! 0 ++
+                                      ". Your second argument is: " ++
+                                      args !! 1
+                                 _ -> "Too many arguments!"
+
 main :: IO ()
 main = do
   args <- getArgs
   putStr "Hello"
-  if length args > 0
-  then putStrLn (", " ++ args !! 0)
-  else putStrLn ""
+  putStrLn $ argumentMessage args
