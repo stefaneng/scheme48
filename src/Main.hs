f00f6d20373ae4c2795355101c8559d5f9095a1b
Stefan Eng <stefaneng13@gmail.com>
2014-07-12 11:01:46 -0700

    First Steps: Exercise 3.
    Prompts for a name, reads the name, and prints out a message along with
    the name.
    
    ```bash
    cabal run
    Please enter your name:
    Stefan
    Hello Stefan
    ```

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,6 @@
 module Main where
 
-import System.Environment (getArgs)
-
-argumentMessage :: [String] -> Int
-argumentMessage args = case length args of
-                         0 -> 0
-                         1 -> read (args !! 0)
-                         _ -> read (args !! 0) + read (args !! 1)
-
 main :: IO ()
 main = do
-  args <- getArgs
-  putStr "Hello "
-  putStrLn $ (show . argumentMessage) args
+  putStrLn "Please enter your name: "
+  getLine >>= putStrLn . (++) "Hello "
