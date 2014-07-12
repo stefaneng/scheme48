8463db8def7548ca559113348576636253916879
Stefan Eng <stefaneng13@gmail.com>
2014-07-12 10:38:36 -0700

    First Steps: Exercise 2
    Run with `cabal run` to add first and second arguments together. If
    there are zero arguments, return zero, and if there is one argument,
    return that argument.

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,14 @@ module Main where
 
 import System.Environment (getArgs)
 
-argumentMessage :: [String] -> String
-argumentMessage args = ", " ++ case length args of
-                                 0 -> ""
-                                 1 -> args !! 0
-                                 2 -> args !! 0 ++
-                                      ". Your second argument is: " ++
-                                      args !! 1
-                                 _ -> "Too many arguments!"
+argumentMessage :: [String] -> Int
+argumentMessage args = case length args of
+                         0 -> 0
+                         1 -> read (args !! 0)
+                         _ -> read (args !! 0) + read (args !! 1)
 
 main :: IO ()
 main = do
   args <- getArgs
-  putStr "Hello"
-  putStrLn $ argumentMessage args
+  putStr "Hello "
+  putStrLn $ (show . argumentMessage) args
