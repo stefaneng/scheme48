4835a817534728a1a00c2a8eace29ba6a96c6e7d
Stefan Eng <stefaneng13@gmail.com>
2014-07-13 20:45:25 -0700

    Parsing: Rewrite parseNumber, using >>=
    Exercise 1, part 2

--- src/Main.hs
+++ src/Main.hs
@@ -34,9 +34,7 @@ parseAtom = do
              _    -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = do
-  result <- many1 digit
-  return $ (Number . read) result
+parseNumber = many1 digit >>= return . Number . read
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
