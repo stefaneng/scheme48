6e1a359cd5d6fdd5f0184c0949ea8d5795d52f0c
Stefan Eng <stefaneng13@gmail.com>
2014-08-01 10:16:32 -0700

    Add library section to cabal file + Instances as module

--- src/Main.hs
+++ src/Main.hs
@@ -7,33 +7,8 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Applicative ((<$>), (<*>))
 import Data.Ratio
 import Language.Scheme.Types
+import Language.Scheme.Types.Instances
 import Data.Complex (Complex((:+)))
-
-instance Show Number where
-    show = showNumber
-
-instance Show LispVal where
-    show = showVal
-
-showNumber :: Number -> String
-showNumber (Integer contents) = show contents
-showNumber (Real contents) = show contents
-showNumber (Rational contents) = show contents
-showNumber (Complex contents) = show contents
-
-showVal :: LispVal -> String
-showVal (String contents) = "\"" ++ contents ++ "\""
-showVal (Atom name) = name
-showVal (Character name) = [name]
-showVal (Number contents) = show contents
-showVal (Bool True) = "#t"
-showVal (Bool False) = "#f"
-showVal (List contents) = "(" ++ unwordsList contents ++ ")"
-showVal (DottedList head' tail') = "(" ++ unwordsList head' ++ " . " ++ showVal tail' ++ ")"
-
-
-unwordsList :: [LispVal] -> String
-unwordsList = unwords . map showVal
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
