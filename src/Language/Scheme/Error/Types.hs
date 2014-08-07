26e34161adf9b52f9b67b1634b9a8233ad820d5c
Stefan Eng <stefaneng13@gmail.com>
2014-08-07 09:44:00 -0700

    Removed orphan instance for errors

--- src/Language/Scheme/Error/Types.hs
+++ src/Language/Scheme/Error/Types.hs
@@ -2,6 +2,7 @@ module Language.Scheme.Error.Types where
 
 import Language.Scheme.Types
 import Text.ParserCombinators.Parsec.Error (ParseError)
+import Language.Scheme.Types.Instances (unwordsList)
 
 data LispError = NumArgs Integer [LispVal]
                | TypeMismatch String LispVal
@@ -10,3 +11,16 @@ data LispError = NumArgs Integer [LispVal]
                | NotFunction String String
                | UnboundVar String String
                | Default String
+
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected
+                                          ++ " args; found values "
+                                          ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                          ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+
+instance Show LispError where show = showError
