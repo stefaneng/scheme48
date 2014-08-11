af2190d05757747b1a14a6eaf4525c175a385e80
Stefan Eng <stefaneng13@gmail.com>
2014-08-11 15:54:02 -0700

    "if" special form

--- src/Language/Scheme/Evaluator.hs
+++ src/Language/Scheme/Evaluator.hs
@@ -10,6 +10,11 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _)   = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", cond, conseq, alt]) = do
+  result <- eval cond
+  case result of
+    Bool False -> eval alt
+    otherwise  -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
